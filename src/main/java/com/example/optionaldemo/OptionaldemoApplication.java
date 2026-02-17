87388475586fae7c0daa2a0bfc3b79091f54eddf
Rajunaidumule <rajusatyavathi48@gmail.com>
2026-02-17 13:38:02 +0530

    RFP-Optional-intial Commit

---
+++ src/main/java/com/example/optionaldemo/OptionaldemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.optionaldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptionaldemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptionaldemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/optionaldemo/OptionaldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.optionaldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptionaldemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
